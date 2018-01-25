7bb69685988fff43999faee354e1c45401e186b7
mazi tar <mazi.tr@gmail.com>
2018-01-25 15:50:52 +0200

    css-last

--- public/googleAPIclient.js
+++ public/googleAPIclient.js
@@ -19,6 +19,7 @@ function initMap() {
         fullscreenControl: false,
         zoomControl: false,
         mapTypeControl: false,
+        streetViewControl: false,
         minZoom: 15
 
     });
